61290ae36e6a70c554ffa4ef63b77676ce10c756
Daniel Cohen <dan@supercore.co.uk>
2015-11-14 14:23:53 +0000

    restage

---
+++ Source/FYP/FYPSpline.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FYP.h"
+#include "FYPSpline.h"
+
+
+// Sets default values
+AFYPSpline::AFYPSpline()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFYPSpline::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFYPSpline::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
