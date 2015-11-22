f2c92d30bc3f3ca62ba1c4a898ed16583bfe2ded
Daniel Cohen <dan@supercore.co.uk>
2015-11-22 18:25:31 +0000

    chunk base cpp class

---
+++ Source/FYP/FYPChunk.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "FYPChunk.generated.h"
+
+UCLASS()
+class FYP_API AFYPChunk : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFYPChunk();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
